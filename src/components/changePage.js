e857e539cabea24ccdd368738219d4bfd862f8e9
mmsmsy <mmsmsy@gmail.com>
2017-06-04 14:04:07 +0200

    List-control buttons are no longer active when at the ends of the list.

--- src/components/changePage.js
+++ src/components/changePage.js
@@ -18,39 +18,57 @@ let changePage = (addition) => {
     // check if it's a call from first/last page buttons
     if (typeof addition === "string") {
       if (addition === "first") {
-        currentParams[1] = currentParams[1]
-          .slice(0, 5)
-          .concat(1);
-
-        connectAxios("http://rt.ex7.pl/get-data", currentParams);
-        $(".list-control-page-num").html("Page 1/" + lastPage);
-
-        return;
+        if (currentPage === 1) return;
+        newPage = 1;
       }
       else if (addition === "last") {
+        if (currentPage === lastPage) return;
         newPage = lastPage;
-
-        currentParams[1] = currentParams[1]
-          .slice(0, 5)
-          .concat(newPage);
-
-        connectAxios("http://rt.ex7.pl/get-data", currentParams);
-        $(".list-control-page-num").html("Page " + newPage + "/" + lastPage);
-
-        return;
       }
     }
-
     // if it's not then it's a call from prev/next page buttons, so determine what should be the following page
-    if (currentPage <= 1) {
-      if (addition < 0) newPage = lastPage;
+    else {
+      if (currentPage <= 1) {
+        if (addition < 0) return;
+        newPage = currentPage + addition;
+      }
+      else if (currentPage >= lastPage) {
+        if (addition > 0) return;
+        newPage = currentPage + addition;
+      }
       else newPage = currentPage + addition;
     }
-    else if (currentPage >= lastPage) {
-      if (addition > 0) newPage = 1;
-      else newPage = currentPage + addition;
+
+    if (newPage === 1) {
+      $(".list-control-prev")
+        .addClass("inactive")
+      $(".list-control-first")
+        .addClass("inactive")
+      $(".list-control-next")
+        .removeClass("inactive")
+      $(".list-control-last")
+        .removeClass("inactive")
     }
-    else newPage = currentPage + addition;
+    else if (newPage === lastPage) {
+      $(".list-control-next")
+        .addClass("inactive")
+      $(".list-control-last")
+        .addClass("inactive")
+      $(".list-control-prev")
+        .removeClass("inactive")
+      $(".list-control-first")
+        .removeClass("inactive")
+    }
+    else {
+      $(".list-control-prev")
+        .removeClass("inactive")
+      $(".list-control-first")
+        .removeClass("inactive")
+      $(".list-control-next")
+        .removeClass("inactive")
+      $(".list-control-last")
+        .removeClass("inactive")
+    }
 
     currentParams[1] = currentParams[1]
       .slice(0, 5)
--- src/main.js
+++ src/main.js
@@ -1,24 +1,18 @@
-import { changePage } from './components/changePage';
-import { changePageSize } from './components/changePage';
+import { changePage, changePageSize } from './components/changePage';
 import { connectAxios, currentParams } from './components/connectAxios';
 
 $(document).ready( () => connectAxios('http://rt.ex7.pl/get-data', currentParams));
 
-// add changePage function to the buttons
 $(".list-control-prev")
   .on("click", () => changePage(-1));
-
-$(".list-control-next")
-  .on("click", () => changePage(1));
-
 $(".list-control-first")
   .on("click", () => changePage("first"));
-
+$(".list-control-next")
+  .on("click", () => changePage(1));
 $(".list-control-last")
   .on("click", () => changePage("last"));
 
 $(".page-size-small")
   .on("click", () => changePageSize(100));
-
 $(".page-size-big")
   .on("click", () => changePageSize(1000));
